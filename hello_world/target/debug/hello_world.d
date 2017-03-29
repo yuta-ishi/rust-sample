25db5848b6cd2a77234c85e27778d908dcd7776e
yuta-ishi <yu.ishi.25@gmail.com>
2017-03-29 16:40:44 +0900

    Initial commit.

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/yuta/workspace/rust_projects/hello_world/target/debug/hello_world: /Users/yuta/workspace/rust_projects/hello_world/src/main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/yuta/workspace/rust_projects/hello_world/target/release/hello_world: /Users/yuta/workspace/rust_projects/hello_world/src/main.rs
